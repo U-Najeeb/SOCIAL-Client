fceb6758df60a39971ae737c23eb28b196472717
Haseeb Qureshi <haseeb.qureshi@iquasar.com>
2023-09-12 20:23:18 +0530

    basic additions +++

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,18 @@ const Login = () => {
   const [name, setName] = React.useState<string>("");
   const [room, setRoom] = React.useState<string>("");
 
+  const HandleLogin = () => {
+    if (name.length > 2) {
+      if (room.length > 2) {
+        console.log("Login successful")
+      } else {
+        alert("Provide a proper Room!");
+      }
+    } else {
+      alert("Provide a proper name!");
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="header">
@@ -26,7 +38,7 @@ const Login = () => {
         />
       </div>
       <div className="button">
-        <button>Login</button>
+        <button onClick={() => HandleLogin()}>Login</button>
       </div>
     </div>
   );
